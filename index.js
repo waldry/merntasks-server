7815ca2bb94104eeb3ae02b2bfbc2e97652b8299
Waldry <waldrymaster@outlook.com>
2020-08-17 00:27:40 -0400

    last try

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ app.use(cors());
 //Habilitar express.json
 app.use(express.json({ extended: true }));
 //Puerto de la app
-const port = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
-//Importar rutas
+//ImPORTar rutas
 app.use("/api/usuarios", require("./routes/usuarios"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/proyectos", require("./routes/proyectos"));
 app.use("/api/tareas", require("./routes/tareas"));
 //modify
 //Arrancar app
-app.listen(port, "0.0.0.0", () => {
-	console.log(`El servidor esta funcionando en el puerto ${port}`);
+app.listen(PORT, "0.0.0.0", () => {
+	console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 });
